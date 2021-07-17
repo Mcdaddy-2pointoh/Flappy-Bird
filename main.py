02d751201ec1338f94d945e15f0691e29bd5f4b1
Mcdaddy-2pointoh <sharvil.d@somaiya.edu>
2021-07-17 14:47:50 +0530

    Screen Template
    Basic display created and frame rate limited

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("HI")
+import pygame as pg
+import sys
+
+pg.init()
+
+# Creates Canvas for display
+screen = pg.display.set_mode((288, 512))
+while True:
+    # Input and pre-defined events
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+
+    # Update adds any image/object on the predefined canvas
+    pg.display.update()
+
